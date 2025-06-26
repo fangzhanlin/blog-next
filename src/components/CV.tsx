52439d8a18cb454db77facadbfb70adfaf08422d
fangzhanlin <934814745@qq.com>
2025-06-26 17:37:12 +0800

    Update CV.tsx

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -94,9 +94,9 @@ function CvItemElement({ cvItem }: { cvItem: CvItem }) {
 export default function CvSectionElement() {
   const personalInfo: PersonalInfo = {
     name: "Fangzhan Lin",
-    email: "xxx@gmail.com",
+    email: "lfz17@tongji.edu.cn",
     location: "Shanghai, China",
-    intro: "XXXXX.",
+    intro: "Doctoral candidate.",
     figurePath: "/personal/kexu_photo.webp",
   };
 
@@ -106,48 +106,19 @@ export default function CvSectionElement() {
       items: [
         {
           title: "Tongji University, Shanghai",
-          subtitle: "Master's Degree in Management Science and Engineering",
+          subtitle: "Integrated Master-PhD Degree in Management Science and Engineering",
           content: "",
-          period: "September 2024 - Present",
+          period: "September 2022 - Present",
         },
         {
-          title: "Sichuan University, Chengdu",
-          subtitle: "Bachelor's Degree in Industrial Engineering",
+          title: "Tongji University, Chengdu",
+          subtitle: "Bachelor's Degree in Information Systems and Information Management",
           content: "",
-          period: "September 2020 - June 2024",
+          period: "September 2018 - June 2022"
         },
       ],
     },
-    {
-      title: "Work Experience",
-      items: [
-        {
-          title: "Airbus Beijing Engineering Centre (ABEC), Beijing",
-          subtitle: "Engineering Intern at ACO1I",
-          content: "Data Mining, Operations System Development",
-          period: "January 2024 - May 2024",
-        },
-        {
-          title: "West China Biomedical Big Data Center, Chengdu",
-          subtitle: "Research Assistant",
-          content: "Active Learning, Contrastive Learning",
-          period: "October 2022 - April 2023",
-        },
-      ],
-    },
-    {
-      title: "Projects",
-      items: [
-        {
-          title:
-            "Design and Control Method of Modular Mechanical Prosthesis, China",
-          subtitle:
-            "College Students' Innovation and Entrepreneurship Competition",
-          content: "National Project",
-          period: "",
-        },
-      ],
-    },
+    
     {
       title: "Publications",
       items: [
@@ -155,21 +126,7 @@ export default function CvSectionElement() {
           title: "",
           subtitle: "",
           content:
-            "Xu, K., Hu, W., & Zhou, Z. (2024). The impact of reward distribution policies on user engagement and service consumption: A natural experiment at a cloud computing service provider. China Association for Information Systems Annual Meeting (CNAIS) 2024.",
-          period: "",
-        },
-        {
-          title: "",
-          subtitle: "",
-          content:
-            "Xu, K., Nie, J., Chen, Y., Ban, Z., Liu, D., & Yin, R. (2024). Predicting intensive care unit length of stay for inflammatory bowel diseases patients using machine learning. In F. Tosi et al. (Eds.), Springer Series in Design and Innovation.",
-          period: "",
-        },
-        {
-          title: "",
-          subtitle: "",
-          content:
-            "Xu, K., Chen, Y., & Nie, J. (2023). Subclass classification of ancient glassware based on K-Means and GMM. Highlights in Science, Engineering and Technology, 42, 277-284.",
+            "Zhongyun, Z., Yuming, G., Fangzhan, L, & Youqing, L. (Under revision). How the Technical Features of Generative AI Address Employee Needs: an Affordance and Disaffordance Lens, Journal of Managerial Psychology.",
           period: "",
         },
       ],
